ebdad1152f4d88018e60ef901cc8ca50fecce720
Aleksa Jovicic <aleksa.j@outlook.com>
2018-12-16 16:51:06 +0100

    Updated tab navigator

--- src/TabNavigator.tsx
+++ src/TabNavigator.tsx
@@ -1,6 +1,6 @@
 import { createBottomTabNavigator, createNavigationContainer, NavigationContainer } from "react-navigation";
 import { AboutScreen } from "./screens/AboutScreen/AboutScreen";
-import { HomeScreen } from "./screens/HomeScreen/HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 const TabNavigator: NavigationContainer = createBottomTabNavigator({
   Home: HomeScreen,
