d9ac4881d559d6b19dd961f2f10dd60ca667f6ad
Anno8 <aleksa.j@outlook.com>
2019-01-18 20:51:36 +0100

    Deleted duplicated folder

--- src/components/toDo/TodoSelectors.ts
+++
@@ -1,5 +0,0 @@
-import { ITodoItem } from "../../models/ITodoItem";
-import { IAppState } from "../../store/IAppState";
-import { REDUCER_KEY } from "./TodoConstants";
-
-export const getTodos = (state: IAppState): ITodoItem[] => state[REDUCER_KEY];
--- src/components/toDo/createToDo/CreateTodoStyle.ts
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from "react-native";
-
-export default StyleSheet.create({
-  container: {
-    fontSize: 16,
-    width: "100%",
-  }
-});
