312a92ebf99a96da703353a2a3a179be603b77f4
Aleksa Jovicic <aleksa.j@outlook.com>
2019-03-10 17:39:29 +0100

    Added test for Todo component

---
+++ src/components/todo/__tests__/Todo.tests.tsx
@@ -0,0 +1,42 @@
+import Todo from "components/todo/Todo";
+import { TodoItem } from "components/todo/todoItem/TodoItem";
+import { shallow, ShallowWrapper } from "enzyme";
+import React from "react";
+
+describe("Todo component test", () => {
+  // Arrange on top as not to repeat it in every test case
+  const mockTodoItem = {
+    finished: false,
+    id: 1,
+    name: "test"
+  };
+  const mockProps = {
+    deleteTodoItem: jest.fn(),
+    todos: [mockTodoItem],
+    toggleTodoItem: jest.fn()
+  };
+
+  it("should render", () => {
+    const wrapper: ShallowWrapper = shallow(<Todo {...mockProps} />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should call toggle todo on child action", () => {
+    const wrapper: ShallowWrapper = shallow(<Todo {...mockProps} />);
+
+    wrapper.find(TodoItem).prop("toggleTodo")(mockTodoItem);
+
+    expect(mockProps.toggleTodoItem).toBeCalled();
+    expect(mockProps.toggleTodoItem).toBeCalledWith(mockTodoItem);
+  });
+
+  it("should call delete todo on child action", () => {
+    const wrapper: ShallowWrapper = shallow(<Todo {...mockProps} />);
+
+    wrapper.find(TodoItem).prop("deleteTodo")(mockTodoItem.id);
+
+    expect(mockProps.deleteTodoItem).toBeCalled();
+    expect(mockProps.deleteTodoItem).toBeCalledWith(mockTodoItem.id);
+  });
+});
