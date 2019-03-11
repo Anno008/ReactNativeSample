9028c8b72b487722e5f5a58bfc6f69f3ce2a0772
Aleksa Jovicic <aleksa.j@outlook.com>
2019-03-11 21:36:50 +0100

    TodoSelectors unit tests

---
+++ src/components/todo/__tests__/TodoSelectors.tests.tsx
@@ -0,0 +1,22 @@
+import { REDUCER_KEY, } from "components/todo/TodoConstants";
+import { getTodos } from "components/todo/TodoSelectors";
+import { ITodoItem } from "models/ITodoItem";
+import { IAppState } from "store/IAppState";
+
+describe("Todo selector tests", () => {
+    const todos: ITodoItem[] = [
+        { id: 1, finished: false, name: "First todo" }
+    ];
+
+    const appState: IAppState = {
+        [REDUCER_KEY]: todos
+    };
+    it("Should return todo array for valid state", () => {
+        const result = getTodos(appState);
+
+        expect(result.length).toEqual(1);
+        expect(result[0].id).toEqual(todos[0].id);
+        expect(result[0].finished).toEqual(todos[0].finished);
+        expect(result[0].name).toEqual(todos[0].name);
+    });
+});
