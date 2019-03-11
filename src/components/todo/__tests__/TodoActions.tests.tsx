7d32d9acf30c6e69edad46f0a1f8c6843cc00fb4
Aleksa Jovicic <aleksa.j@outlook.com>
2019-03-11 20:58:33 +0100

    Written unit tests for TodoActions

---
+++ src/components/todo/__tests__/TodoActions.tests.tsx
@@ -0,0 +1,52 @@
+import { addTodoItem, deleteTodoItem, toggleTodoItem} from "components/todo/TodoActions";
+import { ADD_TODO, DELETE_TODO, TOGGLE_TODO } from "components/todo/TodoConstants";
+
+describe("addTodoItem tests", () => {
+  it("should be a function", () => {
+   expect(typeof addTodoItem).toBe("function");
+  });
+
+  it("should return object with payload and type", () => {
+      const result = addTodoItem("test");
+
+      expect(result).toEqual({
+          payload: "test",
+          type: ADD_TODO
+      });
+  });
+});
+
+describe("deleteTodoItem tests", () => {
+    it("should be a function", () => {
+     expect(typeof deleteTodoItem).toBe("function");
+    });
+
+    it("should return object with payload and type", () => {
+        const result = deleteTodoItem(1);
+
+        expect(result).toEqual({
+            payload: 1,
+            type: DELETE_TODO
+        });
+    });
+  });
+
+describe("toggleTodoItem tests", () => {
+    it("should be a function", () => {
+     expect(typeof toggleTodoItem).toBe("function");
+    });
+
+    it("should return object with payload and type", () => {
+        const todoItem = {
+            finished: true,
+            id: 1,
+            name: "test"
+        };
+        const result = toggleTodoItem(todoItem);
+
+        expect(result).toEqual({
+            payload: todoItem,
+            type: TOGGLE_TODO
+        });
+    });
+  });
